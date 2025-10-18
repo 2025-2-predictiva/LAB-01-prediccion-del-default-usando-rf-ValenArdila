6448471a043679dd5015e87e2dac7cc9f93c035c
Valentina <vardilaq@unal.edu.co>
2025-10-17 23:09:29 -0500

    Lab Completa

---
+++ homework/resume.py
@@ -0,0 +1,256 @@
+# flake8: noqa: E501
+#
+# En este dataset se desea pronosticar el default (pago) del cliente el próximo
+# mes a partir de 23 variables explicativas.
+#
+#   LIMIT_BAL: Monto del credito otorgado. Incluye el credito individual y el
+#              credito familiar (suplementario).
+#         SEX: Genero (1=male; 2=female).
+#   EDUCATION: Educacion (0=N/A; 1=graduate school; 2=university; 3=high school; 4=others).
+#    MARRIAGE: Estado civil (0=N/A; 1=married; 2=single; 3=others).
+#         AGE: Edad (years).
+#       PAY_0: Historia de pagos pasados. Estado del pago en septiembre, 2005.
+#       PAY_2: Historia de pagos pasados. Estado del pago en agosto, 2005.
+#       PAY_3: Historia de pagos pasados. Estado del pago en julio, 2005.
+#       PAY_4: Historia de pagos pasados. Estado del pago en junio, 2005.
+#       PAY_5: Historia de pagos pasados. Estado del pago en mayo, 2005.
+#       PAY_6: Historia de pagos pasados. Estado del pago en abril, 2005.
+#   BILL_AMT1: Historia de pagos pasados. Monto a pagar en septiembre, 2005.
+#   BILL_AMT2: Historia de pagos pasados. Monto a pagar en agosto, 2005.
+#   BILL_AMT3: Historia de pagos pasados. Monto a pagar en julio, 2005.
+#   BILL_AMT4: Historia de pagos pasados. Monto a pagar en junio, 2005.
+#   BILL_AMT5: Historia de pagos pasados. Monto a pagar en mayo, 2005.
+#   BILL_AMT6: Historia de pagos pasados. Monto a pagar en abril, 2005.
+#    PAY_AMT1: Historia de pagos pasados. Monto pagado en septiembre, 2005.
+#    PAY_AMT2: Historia de pagos pasados. Monto pagado en agosto, 2005.
+#    PAY_AMT3: Historia de pagos pasados. Monto pagado en julio, 2005.
+#    PAY_AMT4: Historia de pagos pasados. Monto pagado en junio, 2005.
+#    PAY_AMT5: Historia de pagos pasados. Monto pagado en mayo, 2005.
+#    PAY_AMT6: Historia de pagos pasados. Monto pagado en abril, 2005.
+#
+# La variable "default payment next month" corresponde a la variable objetivo.
+#
+# El dataset ya se encuentra dividido en conjuntos de entrenamiento y prueba
+# en la carpeta "files/input/".
+#
+# Los pasos que debe seguir para la construcción de un modelo de
+# clasificación están descritos a continuación.
+#
+#
+# Paso 1.
+# Realice la limpieza de los datasets:
+# - Renombre la columna "default payment next month" a "default".
+# - Remueva la columna "ID".
+# - Elimine los registros con informacion no disponible.
+# - Para la columna EDUCATION, valores > 4 indican niveles superiores
+#   de educación, agrupe estos valores en la categoría "others".
+# - Renombre la columna "default payment next month" a "default"
+# - Remueva la columna "ID".
+#
+#
+# Paso 2.
+# Divida los datasets en x_train, y_train, x_test, y_test.
+#
+#
+# Paso 3.
+# Cree un pipeline para el modelo de clasificación. Este pipeline debe
+# contener las siguientes capas:
+# - Transforma las variables categoricas usando el método
+#   one-hot-encoding.
+# - Ajusta un modelo de bosques aleatorios (rando forest).
+#
+#
+# Paso 4.
+# Optimice los hiperparametros del pipeline usando validación cruzada.
+# Use 10 splits para la validación cruzada. Use la función de precision
+# balanceada para medir la precisión del modelo.
+#
+#
+# Paso 5.
+# Guarde el modelo (comprimido con gzip) como "files/models/model.pkl.gz".
+# Recuerde que es posible guardar el modelo comprimido usanzo la libreria gzip.
+#
+#
+# Paso 6.
+# Calcule las metricas de precision, precision balanceada, recall,
+# y f1-score para los conjuntos de entrenamiento y prueba.
+# Guardelas en el archivo files/output/metrics.json. Cada fila
+# del archivo es un diccionario con las metricas de un modelo.
+# Este diccionario tiene un campo para indicar si es el conjunto
+# de entrenamiento o prueba. Por ejemplo:
+#
+# {'dataset': 'train', 'precision': 0.8, 'balanced_accuracy': 0.7, 'recall': 0.9, 'f1_score': 0.85}
+# {'dataset': 'test', 'precision': 0.7, 'balanced_accuracy': 0.6, 'recall': 0.8, 'f1_score': 0.75}
+#
+#
+# Paso 7.
+# Calcule las matrices de confusion para los conjuntos de entrenamiento y
+# prueba. Guardelas en el archivo files/output/metrics.json. Cada fila
+# del archivo es un diccionario con las metricas de un modelo.
+# de entrenamiento o prueba. Por ejemplo:
+#
+# {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
+# {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
+#
+
+import gzip
+import json
+import os
+import pickle
+import zipfile
+from glob import glob
+
+import pandas as pd  # type: ignore
+from sklearn.compose import ColumnTransformer  # type: ignore
+from sklearn.ensemble import RandomForestClassifier  # type: ignore
+from sklearn.metrics import (  # type: ignore
+    balanced_accuracy_score,
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    recall_score,
+)
+from sklearn.model_selection import GridSearchCV  # type: ignore
+from sklearn.pipeline import Pipeline  # type: ignore
+from sklearn.preprocessing import OneHotEncoder  # type: ignore
+
+
+def load_data(input_directory):
+    """Read zip files in a directory and store them in dfs"""
+    dfs = []
+
+    routes = glob(f"{input_directory}/*")
+
+    for route in routes:
+        with zipfile.ZipFile(f"{route}", mode="r") as zf:
+            for fn in zf.namelist():
+
+                with zf.open(fn) as f:
+                    dfs.append(pd.read_csv(f, sep=",", index_col=0))
+
+    return dfs
+
+
+def _create_output_directory(output_directory):
+    if os.path.exists(output_directory):
+        for file in glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
+
+
+def clean_df(df):
+    """Paso 1: Clean dataset"""
+    df = df.copy()
+    df = df.rename(columns={"default payment next month": "default"})
+    df = df.loc[df["MARRIAGE"] != 0]
+    df = df.loc[df["EDUCATION"] != 0]
+    df["EDUCATION"] = df["EDUCATION"].apply(lambda x: 4 if x >= 4 else x)
+    return df.dropna()
+    
+
+def split_data(df):
+    """Paso 2: feats & Target"""
+    return df.drop(columns=["default"]), df["default"]
+
+
+def create_pipeline():
+    """Paso 3: Create processing pipeline"""
+    cat_features = ["SEX", "EDUCATION", "MARRIAGE"]
+    preprocessor = ColumnTransformer(
+        [("cat", OneHotEncoder(handle_unknown="ignore"), cat_features)],
+        remainder="passthrough",
+    )
+    return Pipeline(
+        [
+            ("preprocessor", preprocessor),
+            ("classifier", RandomForestClassifier(random_state=42)),
+        ]
+    )
+
+
+def create_estimator(pipeline):
+    """Pipeline"""
+    param_grid = {
+        "classifier__n_estimators": [100, 200, 500],
+        "classifier__max_depth": [None, 5, 10],
+        "classifier__min_samples_split": [2, 5],
+        "classifier__min_samples_leaf": [1, 2],
+    }
+
+    return GridSearchCV(
+        pipeline,
+        param_grid,
+        cv=10,
+        scoring="balanced_accuracy",
+        n_jobs=-1,
+        refit=True,
+        verbose=2,
+    )
+
+
+def _save_model(path, estimator):
+    _create_output_directory("files/models/")
+
+    with gzip.open(path, "wb") as f:
+        pickle.dump(estimator, f)
+
+
+def calculate_metrics(dataset_type, y_true, y_pred):
+    """Calculate metrics"""
+    return {
+        "type": "metrics",
+        "dataset": dataset_type,
+        "precision": precision_score(y_true, y_pred, zero_division=0),
+        "balanced_accuracy": balanced_accuracy_score(y_true, y_pred),
+        "recall": recall_score(y_true, y_pred, zero_division=0),
+        "f1_score": f1_score(y_true, y_pred, zero_division=0),
+    }
+
+
+def calculate_confusion(dataset_type, y_true, y_pred):
+    """Confusion matrix"""
+    cm = confusion_matrix(y_true, y_pred)
+    return {
+        "type": "cm_matrix",
+        "dataset": dataset_type,
+        "true_0": {"predicted_0": int(cm[0][0]), "predicted_1": int(cm[0][1])},
+        "true_1": {"predicted_0": int(cm[1][0]), "predicted_1": int(cm[1][1])},
+    }
+
+
+def _run_jobs():
+    test_df, train_df = [clean_df(df) for df in load_data("files/input")]
+
+    x_train, y_train = split_data(train_df)
+    x_test, y_test = split_data(test_df)
+
+    pipeline = create_pipeline()
+
+    estimator = create_estimator(pipeline)
+    estimator.fit(x_train, y_train)
+
+    _save_model(
+        os.path.join("files/models/", "model.pkl.gz"),
+        estimator,
+    )
+
+    y_test_pred = estimator.predict(x_test)
+    test_precision_metrics = calculate_metrics("test", y_test, y_test_pred)
+    y_train_pred = estimator.predict(x_train)
+    train_precision_metrics = calculate_metrics("train", y_train, y_train_pred)
+
+    test_confusion_metrics = calculate_confusion("test", y_test, y_test_pred)
+    train_confusion_metrics = calculate_confusion("train", y_train, y_train_pred)
+
+    os.makedirs("files/output/", exist_ok=True)
+
+    with open("files/output/metrics.json", "w", encoding="utf-8") as file:
+        file.write(json.dumps(train_precision_metrics) + "\n")
+        file.write(json.dumps(test_precision_metrics) + "\n")
+        file.write(json.dumps(train_confusion_metrics) + "\n")
+        file.write(json.dumps(test_confusion_metrics) + "\n")
+
+
+if __name__ == "__main__":
+    _run_jobs()
